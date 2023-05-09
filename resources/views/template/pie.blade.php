6afd1d13e987d8bb03afb6d4560dfad1bd88dd8d
josueevans26 <jevangelistag@toluca.tecnm.mx>
2023-05-08 22:02:47 -0600

    cambio en la ruta de los scripts2

--- resources/views/template/pie.blade.php
+++ resources/views/template/pie.blade.php
@@ -26,15 +26,17 @@
 <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js"
     integrity="sha256-a2yjHM4jnF9f54xUQakjZGaqYs/V1CYvWpoqZzC2/Bw=" crossorigin="anonymous"></script>
 
-<script src="https://taller-production.up.railway.appestilo/assets/js/isotope.min.js"></script>
-<script src="https://taller-production.up.railway.appestilo/assets/js/owl-carousel.js"></script>
-<script src="https://taller-production.up.railway.appestilo/assets/js/lightbox.js"></script>
-<script src="https://taller-production.up.railway.appestilo/assets/js/tabs.js"></script>
-<script src="https://taller-production.up.railway.appestilo/assets/js/video.js"></script>
-<script src="https://taller-production.up.railway.appestilo/assets/js/slick-slider.js"></script>
-<script src="https://taller-production.up.railway.appestilo/assets/js/custom.js"></script>
+<script src="https://taller-production.up.railway.app/estilo/assets/js/isotope.min.js"></script>
+<script src="https://taller-production.up.railway.app/estilo/assets/js/owl-carousel.js"></script>
+<script src="https://taller-production.up.railway.app/estilo/assets/js/lightbox.js"></script>
+<script src="https://taller-production.up.railway.app/estilo/assets/js/tabs.js"></script>
+<script src="https://taller-production.up.railway.app/estilo/assets/js/video.js"></script>
+<script src="https://taller-production.up.railway.app/estilo/assets/js/slick-slider.js"></script>
+<script src="https://taller-production.up.railway.app/estilo/assets/js/custom.js"></script>
+
 {{-- <script src="{!! asset('estilo/assets/js/cambia.js') !!}"></script> --}}
 {{-- Agregar a public --}}
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
 </script>
